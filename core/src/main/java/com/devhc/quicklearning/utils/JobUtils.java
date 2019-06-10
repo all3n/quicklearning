e0772e126f23dc5fc15375e67e4ea3cb0f0673f9
all3nwhc <wanghch8398@163.com>
2019-06-10 18:25:19 +0800

    update client submit

---
+++ core/src/main/java/com/devhc/quicklearning/utils/JobUtils.java
@@ -0,0 +1,100 @@
+package com.devhc.quicklearning.utils;
+
+import com.google.common.collect.Lists;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Arrays;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.permission.FsPermission;
+import org.apache.hadoop.yarn.api.records.URL;
+import org.apache.hadoop.yarn.factory.providers.RecordFactoryProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JobUtils {
+  private static Logger LOG = LoggerFactory.getLogger(JobUtils.class);
+  public static final FsPermission TEMP_PERM = new FsPermission("777");
+  public static final String HDFS_USER_ROOT = "/user";
+  public static final String HDFS_XDL_WORK_DIR = ".quicklearning";
+  public static final String HDFS_PYTHON_DIR = "python";
+
+  public static String getName(String filePath) {
+    return new Path(filePath).getName();
+  }
+  public static String getCurUser() {
+    return System.getProperty("user.name");
+  }
+
+
+  public static String genAppBasePath(
+      Configuration conf, String appId, String user) throws IOException {
+    String basePath = String.format("%s/%s/%s/%s",
+        HDFS_USER_ROOT, user, HDFS_XDL_WORK_DIR, appId);
+    if (basePath.startsWith("/")) {
+      basePath = basePath.substring(1);
+    }
+    String defaultFs = conf.get("fs.defaultFS");
+    basePath = defaultFs + basePath + "/";
+    FileSystem fs = FileSystem.get(conf);
+    Path appStagePath = new Path(basePath);
+    if (!fs.exists(appStagePath)) {
+      fs.mkdirs(appStagePath, TEMP_PERM);
+      fs.setPermission(appStagePath, TEMP_PERM);
+      LOG.info("Path:[{}] not exists, create success.", appStagePath);
+    }
+    return basePath;
+  }
+
+
+
+
+  public static ArrayList<String> splitString(String value, String seperator) {
+    if (value.isEmpty()) {
+      return Lists.newArrayList();
+    }
+    return new ArrayList<String>(Arrays.asList(value.split(seperator)));
+  }
+
+
+  public static void runCmd(String cmd) {
+    String[] cmdArr = cmd.split(" ");
+    Runtime run = Runtime.getRuntime();
+    try {
+      Process p = run.exec(cmdArr);
+      p.waitFor();
+      if (p.exitValue() == 0) {
+        LOG.info("Run cmd [" + cmd + "] success.");
+        return;
+      } else {
+        BufferedReader read = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+        String result = read.readLine();
+        throw new RuntimeException("Run cmd [" + cmd + "] failed, " + result);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+      throw new RuntimeException(e.getMessage());
+    }
+  }
+
+
+  public static URL fromURI(URI uri, Configuration conf) {
+    URL url =
+        RecordFactoryProvider.getRecordFactory(conf).newRecordInstance(
+            URL.class);
+    if (uri.getHost() != null) {
+      url.setHost(uri.getHost());
+    }
+    if (uri.getUserInfo() != null) {
+      url.setUserInfo(uri.getUserInfo());
+    }
+    url.setPort(uri.getPort());
+    url.setScheme(uri.getScheme());
+    url.setFile(uri.getPath());
+    return url;
+  }
+}
---
+++ scripts/submit.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+export APP_DIR=`cd "$(dirname $0)/../"; pwd`
+
+APP_JAR=`ls $APP_DIR/*.jar|head -n 1`
+
+java -cp $APP_DIR/libs/*:$APP_DIR/*:`hadoop classpath` com.devhc.quicklearning.client.Client "$@"
+
+
